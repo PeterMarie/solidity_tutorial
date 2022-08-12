5716b7974eea2d1586b1b826d2db90af7a4cb416
Peter Marie <peterogwara@gmail.com>
2022-08-12 16:33:07 +0100

    Update fundme.sol

--- fundme.sol
+++ fundme.sol
@@ -2,57 +2,37 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-//import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-} 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
     mapping(address=>uint) public senderaddresstoamontvalue;
-    AggregatorV3Interface pricefeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
 
     function fund() public payable {
         senderaddresstoamontvalue[msg.sender] += msg.value;
     }
 
     function getVersion() public view returns(uint){
-        return pricefeed.version();
+        return AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e).version();
     }
 
     function getPrice() public view returns(uint){
-        (,int256 answer,,,) = pricefeed.latestRoundData();
-            return uint(answer);
+        (,int256 answer,,,) = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e).latestRoundData();
+        uint8 decimal = uint8(getDecimals());
+        uint8 multiplier = uint8(10 ** (18 - decimal));
+        return uint(answer * multiplier);
     }
 
     function getDecimals() public view returns(uint8){
-        return pricefeed.decimals();
+      AggregatorV3Interface pricefeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+      return pricefeed.decimals();
+    }
+    
+    function getEtherPrice(uint weiPrice) public pure returns(uint){
+      return (weiPrice / (10 ** 18));
+    }
+    
+    function getConversionRate(uint ethAmount) public view returns(uint){
+      return (ethAmount * getEtherPrice(getPrice()));
     }
 }
